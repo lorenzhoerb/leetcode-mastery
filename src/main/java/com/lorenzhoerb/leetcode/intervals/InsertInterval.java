0d9ec6802d78b5457091779143409c1b2a1cc69c
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-08-17 19:32:34 +0200

    add insert interval

--- src/main/java/com/lorenzhoerb/leetcode/intervals/InsertInterval.java
+++ src/main/java/com/lorenzhoerb/leetcode/intervals/InsertInterval.java
@@ -1,5 +1,8 @@
 package com.lorenzhoerb.leetcode.intervals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 57. Insert Interval
  * <p>
@@ -12,4 +15,46 @@ package com.lorenzhoerb.leetcode.intervals;
  * Note that you don't need to modify intervals in-place. You can make a new array and return it.
  */
 public class InsertInterval {
+    public static void main(String[] args) {
+        int[][] result = insert(new int[][]{{2, 5}, {6, 9}}, new int[]{3, 5});
+
+        System.out.println("a");
+    }
+
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        int low = 0;
+        int high = intervals.length - 1;
+        int mid = 0;
+
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            int curStart = intervals[mid][0];
+            int targetStart = newInterval[0];
+            if (curStart == targetStart) {
+                intervals[mid][1] = Math.max(intervals[mid][1], newInterval[1]);
+                return intervals;
+            } else if (curStart < targetStart) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+
+        List<int[]> insertedIntervals = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            if(i == mid) {
+                while (i < intervals.length && intervals[i][1] < newInterval[1]) {
+                   i++;
+                }
+                int[] newly = new int[]{Math.min(newInterval[0], intervals[mid][0]), Math.max(newInterval[1], intervals[i][1])};
+                insertedIntervals.add(newly);
+            } else {
+                insertedIntervals.add(intervals[i]);
+            }
+        }
+
+        return insertedIntervals.toArray(new int[insertedIntervals.size()][]);
+    }
+
 }
