cc01009db5dcb4ec9eefc16404b22c6d3b25a8a5
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2024-08-28 15:27:50 +0200

    Change sudoku example

--- src/main/java/com/lorenzhoerb/leetcode/backtracking/sudoku/SudokuApp.java
+++ src/main/java/com/lorenzhoerb/leetcode/backtracking/sudoku/SudokuApp.java
@@ -3,8 +3,8 @@ package com.lorenzhoerb.leetcode.backtracking.sudoku;
 public class SudokuApp {
     public static void main(String[] args) {
         SudokuBoard board = new SudokuBoard(new Integer[][]{
-                {8, null, null, 4, 1, null, 9, null, null},
-                {null, 2, null, null, null, null, 1, null, 5},
+                {8, 7, null, 4, 1, null, 9, null, null},
+                {null, 2, null, 7, null, null, 1, null, 5},
                 {null, 6, null, null, 2, null, null, 7, 4},
                 {4, null, null, null, 7, null, null, null, null},
                 {null, 5, 2, null, null, null, null, 4, null},
